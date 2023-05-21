96d92b855d196eae375b43c70473b6b1c300f767
nithinsenthil <nithin020304@gmail.com>
2023-05-21 00:21:19 -0700

    Update on front end + trial connection with flask

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from flask import Flask, request
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 FlaskJSON(app)
+CORS(app)
 
-
-@app.route("/testConnection")
+@app.route("/api/testConnection")
 def testConnection():
-    return "<p>Server connection works</p>"
+    rv = "<p>Server connection works</p>"
+    return json_response(200)
 
+# headers={"Access-Control-Allow-Origin": "true"}
+# document.getElementById('increaseBtn').onclick = async function() {
+#   const response = await fetch("http://localhost:8000/api/testConnection");
+#   const jsonData = await response.json();
+#   console.log(jsonData);
+#   //count += 1;
+#   //document.getElementById("countLabel").innerHTML = count;
+# }
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -27,3 +38,5 @@ def submit():
 @app.route('/generateAnalysis')
 def generateAnalysis():
     return "Analysis generated\n"
+
+app.run(port=8000, debug=True)
