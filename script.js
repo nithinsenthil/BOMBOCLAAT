d8276df01937ca5be2f815ac31c1238a3aa975c8
nithinsenthil <nithin020304@gmail.com>
2023-05-21 01:02:15 -0700

    Add front end files with js2Flask info

---
+++ script.js
@@ -0,0 +1,51 @@
+let count = 0;
+
+
+document.getElementById('increaseBtn').onclick = function() {
+  count += 1;
+  document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById('decreaseBtn').onclick = function() {
+  count -= 1;
+  document.getElementById("countLabel").innerHTML = count;
+}
+
+
+
+
+
+let count2 = 0;
+document.getElementById('increaseBtn2').onclick = function() {
+  count2 += 1;
+  document.getElementById("countLabel2").innerHTML = count2;
+}
+
+document.getElementById('decreaseBtn2').onclick = function() {
+  count2 -= 1;
+  document.getElementById("countLabel2").innerHTML = count2;
+}
+
+
+let count3 = 0;
+document.getElementById('increaseBtn3').onclick = function() {
+  count3 += 1;
+  document.getElementById("countLabel3").innerHTML = count3;
+}
+
+document.getElementById('decreaseBtn3').onclick = function() {
+  count3 -= 1;
+  document.getElementById("countLabel3").innerHTML = count3;
+}
+
+
+let count4 = 0;
+document.getElementById('increaseBtn4').onclick = function() {
+  count4 += 1;
+  document.getElementById("countLabel4").innerHTML = count4;
+}
+
+document.getElementById('decreaseBtn4').onclick = function() {
+  count4 -= 1;
+  document.getElementById("countLabel4").innerHTML = count4;
+}
