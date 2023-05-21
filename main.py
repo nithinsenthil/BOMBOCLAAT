fb7c629ed466485f9e6eab75a180703fb184b37d
nithinsenthil <nithin020304@gmail.com>
2023-05-21 03:48:51 -0700

    Full front and back integration

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask_json import FlaskJSON, JsonError, json_response, as_json
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from flask_cors import CORS, cross_origin
-
+from datetime import date, time
+import bson
 
 # Start server
 app = Flask(__name__)
@@ -14,35 +15,95 @@ CORS(app)
 # Connect to MongoDB cluster
 uri = "mongodb+srv://NithinSenthil:BOMBOCLAAT@aggiereuseinventory.mesyrt5.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
+logs = client["Logs"]
+masterCol = (client["Master"])["MasterLog"]
 
 
 # Server/Database connection testing
 @app.route("/api/testConnection")
 def testConnection():
-    rv = "<p>Server connection works</p>"
+    # rv = "<p>Server connection works</p>"
+
+    rv = None
+    oldMasterLog = (masterCol.find())
+    for doc in oldMasterLog:
+        rv = doc
+        break
+
+    for i in rv:
+        print(rv[i])
+
+    # for i in logDict:
+    #     print(logDict[i])
+
     return json_response(200)
 
 
-
-@app.route('/api/submit', methods=['POST'])
+@app.route("/api/submit/", methods=["POST"])
 def submit():
     # We use 'force' to skip mimetype checking to have shorter curl command.
     data = request.get_json(force=True)
     try:
-        numShirts = int(data['shirts'])
+        numShirt = int(data['shirt'])
+        numTanktop = int(data['tankTop'])
+        numLongSleeve = int(data['longSleeve'])
+        numJacket = int(data['jacket'])
+        numPants = int(data['pants'])
+        numJeans = int(data['jeans'])
+        numShorts = int(data['shorts'])
+        numSkirt = int(data['skirt'])
+        totalIn = int(data['totalIn'])
+        totalOut = int(data['totalOut'])
+
     except (KeyError, TypeError, ValueError):
         raise JsonError(description='Invalid value.')
 
-    print("There are " + str(numShirts) + " shirts.")
+    dayLog = logs[str(date.today())]
 
-    return json_response(200, status="Upload Confirmed")
+    tempLog = {
+        "shirt": numShirt,
+        "tankTop": numTanktop,
+        "longSleeve": numLongSleeve,
+        "jacket": numJacket,
+        "pants": numPants,
+        "jeans": numJeans,
+        "shorts": numShorts,
+        "skirt": numSkirt,
+        "totalIn": totalIn,
+        "totalOut": totalOut
+    }
+
+    oldMasterLog = (masterCol.find())
+    for doc in oldMasterLog:
+        rv = doc
+
+
+    masterTempLog = {
+        "shirt": numShirt + rv["shirt"],
+        "tankTop": numTanktop + rv["tankTop"],
+        "longSleeve": numLongSleeve + rv["longSleeve"],
+        "jacket": numJacket + rv["jacket"],
+        "pants": numPants + rv["pants"],
+        "jeans": numJeans + rv["jeans"],
+        "shorts": numShorts + rv["shorts"],
+        "skirt": numSkirt + rv["skirt"],
+        "totalIn": totalIn + rv["totalIn"],
+        "totalOut": totalOut + rv["totalOut"]
+    }
+
+    status1 = dayLog.insert_one(tempLog)
+    status2 = masterCol.insert_one(masterTempLog)
+
+    if status1 and status2:
+        return json_response(200, status="Upload Confirmed")
+    return json_response(200, status="Error could not update database")
 
 
 # Pull data for analysis
 @app.route('/generateAnalysis')
 def generateAnalysis():
 
-    return "Feature Currently Unavailable\n"
+    return "Coming soon to stores near you!\n"
 
 app.run(port=8000, debug=True)
 
--- script.js
+++ script.js
@@ -1,51 +1,173 @@
+let totalIn = 0;
+let totalOut=0;
+
 let count = 0;
-
-
 document.getElementById('increaseBtn').onclick = function() {
   count += 1;
+  totalIn += 1;
   document.getElementById("countLabel").innerHTML = count;
+  document.getElementById("totalcount").innerHTML = totalIn;
 }
 
 document.getElementById('decreaseBtn').onclick = function() {
   count -= 1;
+  totalOut += 1;
   document.getElementById("countLabel").innerHTML = count;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
 }
-
-
-
-
 
 let count2 = 0;
 document.getElementById('increaseBtn2').onclick = function() {
   count2 += 1;
+  totalIn += 1;
   document.getElementById("countLabel2").innerHTML = count2;
+  document.getElementById("totalcount").innerHTML = totalIn;
 }
 
 document.getElementById('decreaseBtn2').onclick = function() {
   count2 -= 1;
+  totalOut += 1;
   document.getElementById("countLabel2").innerHTML = count2;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
 }
-
 
 let count3 = 0;
 document.getElementById('increaseBtn3').onclick = function() {
   count3 += 1;
+  totalIn += 1;
   document.getElementById("countLabel3").innerHTML = count3;
+  document.getElementById("totalcount").innerHTML = totalIn;
 }
 
 document.getElementById('decreaseBtn3').onclick = function() {
   count3 -= 1;
+  totalOut += 1;
   document.getElementById("countLabel3").innerHTML = count3;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
 }
 
 
 let count4 = 0;
 document.getElementById('increaseBtn4').onclick = function() {
   count4 += 1;
+  totalIn += 1;
   document.getElementById("countLabel4").innerHTML = count4;
+  document.getElementById("totalcount").innerHTML = totalIn;
 }
 
 document.getElementById('decreaseBtn4').onclick = function() {
   count4 -= 1;
+  totalOut += 1;
   document.getElementById("countLabel4").innerHTML = count4;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
 }
+
+
+
+
+let count5 = 0;
+document.getElementById('increaseBtn5').onclick = function() {
+  count5 += 1;
+  totalIn += 1;
+  document.getElementById("countLabel5").innerHTML = count5;
+  document.getElementById("totalcount").innerHTML = totalIn;
+}
+
+document.getElementById('decreaseBtn5').onclick = function() {
+  count5 -= 1;
+  totalOut += 1;
+  document.getElementById("countLabel5").innerHTML = count5;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
+}
+
+let count6 = 0;
+document.getElementById('increaseBtn6').onclick = function() {
+  count6 += 1;
+  totalIn += 1;
+  document.getElementById("countLabel6").innerHTML = count6;
+  document.getElementById("totalcount").innerHTML = totalIn;
+}
+
+document.getElementById('decreaseBtn6').onclick = function() {
+  count6 -= 1;
+  totalOut += 1;
+  document.getElementById("countLabel6").innerHTML = count6;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
+}
+
+let count7 = 0;
+document.getElementById('increaseBtn7').onclick = function() {
+  count7 += 1;
+  totalIn += 1;
+  document.getElementById("countLabel7").innerHTML = count7;
+  document.getElementById("totalcount").innerHTML = totalIn;
+}
+
+document.getElementById('decreaseBtn7').onclick = function() {
+  count7 -= 1;
+  totalOut += 1;
+  document.getElementById("countLabel7").innerHTML = count7;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
+}
+
+let count8 = 0;
+document.getElementById('increaseBtn8').onclick = function() {
+  count8 += 1;
+  totalIn += 1;
+  document.getElementById("countLabel8").innerHTML = count8;
+  document.getElementById("totalcount").innerHTML = totalIn;
+}
+
+document.getElementById('decreaseBtn8').onclick = function() {
+  count8 -= 1;
+  totalOut += 1;
+  document.getElementById("countLabel8").innerHTML = count8;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
+}
+
+document.getElementById('submit').onclick = async function() {
+  const response = await fetch("http://localhost:8000/api/submit/", {
+        method: "POST",
+        mode: 'cors',
+        crossorigin: 'true',
+        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "http://127.0.0.1:5000", 'Access-Control-Allow-Credentials': 'true' },
+        body: JSON.stringify(
+          {
+            "shirt": count,
+            "tankTop": count2,
+            "longSleeve": count3,
+            "jacket": count4,
+            "pants": count5,
+            "jeans": count6,
+            "shorts": count7,
+            "skirt": count8,
+            "totalIn": totalIn,
+            "totalOut": totalOut
+          })
+      });
+  const jsonData = await response.json();
+  console.log(jsonData);
+  count=0;
+  count2=0;
+  count3=0;
+  count4=0;
+  count5=0;
+  count6=0;
+  count7=0;
+  count8=0;
+  totalIn=0;
+  totalOut=0;
+  document.getElementById("countLabel").innerHTML = count;
+  document.getElementById("countLabel2").innerHTML = count2;
+  document.getElementById("countLabel3").innerHTML = count3;
+  document.getElementById("countLabel4").innerHTML = count4;
+  document.getElementById("countLabel5").innerHTML = count5;
+  document.getElementById("countLabel6").innerHTML = count6;
+  document.getElementById("countLabel7").innerHTML = count7;
+  document.getElementById("countLabel8").innerHTML = count8;
+  document.getElementById("totalcount").innerHTML = totalIn;
+  document.getElementById("totalcountOut").innerHTML = totalOut;
+
+}
+
+
